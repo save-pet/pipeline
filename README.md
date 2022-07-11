958f657437578be97aaca5a68d54fc02ed5df3a8
zinger <wlstj9964@gmail.com>
2022-07-11 19:02:40 +0900

    ♻️ 코드 schema 에 맞춰서 수정

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # openApi_rescue
+
+openapi_upr_cd.py -> 
--- openapi.py
+++ openapi.py
@@ -1,6 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
-import pandas as pd
+# import pandas as pd
 from pymongo import MongoClient
 from dotenv import load_dotenv
 import os
@@ -9,33 +9,43 @@ load_dotenv()
 
 mongodb_URL = os.environ.get('mongodbURL')
 client = MongoClient(mongodb_URL)
-db = client.openApi
+db = client.test
 
 API_Key = os.environ.get("ApiKey")
 InputDate = int(input("Date : "  ))
 EndDate = InputDate + 10
 page = 1
-URL = "http://apis.data.go.kr/1543061/abandonmentPublicSrvc/abandonmentPublic?bgnde={Date}&endde={endDate}&pageNo={Page}&numOfRows=1000&serviceKey={API}".format(Page=page,Date=InputDate,endDate = EndDate, API=API_Key)
+URL = "http://apis.data.go.kr/1543061/abandonmentPublicSrvc/abandonmentPublic?bgnde={Date}&endde={endDate}&pageNo={Page}&numOfRows=1000&serviceKey={API}".format(Page=page,Date=InputDate, endDate = EndDate, API=API_Key)
 
 rq = requests.get(URL)
 soup = BeautifulSoup(rq.text, "html.parser")
 
-rescue= []
 totalCnt = int(soup.find("totalcount").text)
 print(totalCnt)
 pageCnt = int(totalCnt/1000) + 1
 print(pageCnt)
 # rescue = []
 for item in soup.find_all("item"):
-    kindCd = item.find("kindcd").text
-    happenDt = item.find("happendt").text
+    desertionNo = item.find("desertionno").text
+    imgUrl = item.find("filename").text
+    happenDate = item.find("happendt").text
+    happenPlace = item.find("happenplace").text
+    kindCd = item.find("kindcd").text    
+    colorCd = item.find("colorcd").text
+    sexCode = item.find("sexcd").text
+    neuteYn = item.find("neuteryn").text
     noticeSdt = item.find("noticesdt").text
     noticeEdt = item.find("noticeedt").text
+    specialMark = item.find("specialmark").text
+    age = item.find("age").text
+    weight = item.find("weight").text
     processState = item.find("processstate").text
-    findplace = item.find("happenplace").text
     careAddr = item.find("careaddr").text
+    careName = item.find("carenm").text
+    careTel = item.find("caretel").text
+    officeTel = item.find("officetel").text
     # rescue.append([kindCd, happenDt, noticeSdt, noticeEdt, processState, findplace, careAddr])
-    db.test.insert_one({"kindCd": kindCd, "happenDt": happenDt, "noticeSdt" : noticeSdt, "noticeEdt":noticeEdt,"processState": processState, "findplace":findplace, "careAddr":careAddr})
+    db.rescues.insert_one({"desertionNo": desertionNo,"imgUrl": imgUrl, "happenDate": happenDate, "happenPlace": happenPlace, "kindCode": kindCd, "colorCode": colorCd, "sexCode": sexCode, "neuteY/N": neuteYn, "noticeStartDate":noticeSdt, "noticeEndDate": noticeEdt, "specialMark": specialMark, "age": age, "weight": weight, "processState": processState, "officeTel": officeTel })
 
 # rescue_df = pd.DataFrame(rescue, columns=["KindCode","HappenDate", "NoticeSDate", "NoticeEdate", "State", "findplace", "Shelter"])
 
@@ -49,17 +59,29 @@ if totalCnt > 1000:
         rq = requests.get(URL)
         soup = BeautifulSoup(rq.text, "html.parser")
 
-        rescue= []
+        # rescue= []
         for item in soup.find_all("item"):
-            kindCd = item.find("kindcd").text
-            happenDt = item.find("happendt").text
+            desertionNo = item.find("desertionno").text
+            imgUrl = item.find("filename").text
+            happenDate = item.find("happendt").text
+            happenPlace = item.find("happenplace").text
+            kindCd = item.find("kindcd").text    
+            colorCd = item.find("colorcd").text
+            sexCode = item.find("sexcd").text
+            neuteYn = item.find("neuteryn").text
             noticeSdt = item.find("noticesdt").text
             noticeEdt = item.find("noticeedt").text
+            specialMark = item.find("specialmark").text
+            age = item.find("age").text
+            weight = item.find("weight").text
             processState = item.find("processstate").text
-            findplace = item.find("happenplace").text
             careAddr = item.find("careaddr").text
+            careName = item.find("carenm").text
+            careTel = item.find("caretel").text
+            officeTel = item.find("officetel").text
             # rescue.append([kindCd, happenDt, noticeSdt, noticeEdt, processState, findplace, careAddr])
-            db.test.insert_one({"kindCd": kindCd, "happenDt": happenDt, "noticeSdt" : noticeSdt, "noticeEdt":noticeEdt,"processState": processState, "findplace":findplace, "careAddr":careAddr});
+            
+            db.rescues.insert_one({"desertionNo": desertionNo,"imgUrl": imgUrl, "happenDate": happenDate, "happenPlace": happenPlace, "kindCode": kindCd, "colorCode": colorCd, "sexCode": sexCode, "neuteY/N": neuteYn, "noticeStartDate":noticeSdt, "noticeEndDate": noticeEdt, "specialMark": specialMark, "age": age, "weight": weight, "processState": processState, "officeTel": officeTel })
 
         # rescue_df = pd.DataFrame(rescue, columns=["KindCode","HappenDate", "NoticeSDate", "NoticeEdate", "State", "findplace", "Shelter"])
 
