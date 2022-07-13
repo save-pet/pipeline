8466c5d71cca1c2ad8229ec4ae62153ca81b79c7
cyberjam <elicejam@gmail.com>
2022-07-14 04:54:13 +0900

    ♻️ 1차 리팩토링 완료

--- openapi.py
+++ openapi.py
@@ -12,33 +12,84 @@ def get_db(database_name):
     db = getattr(client, database_name)
     return db
 
-def get_url(API_Key, ten_days_ago, page_number = 1):
-    URL = f"http://apis.data.go.kr/1543061/abandonmentPublicSrvc/abandonmentPublic?bgnde={ten_days_ago}&numOfRows=1000&pageNo={page_number}&serviceKey={API_Key}&_type=json"
+def get_url(API_Key, date, page_number = 1):
+    URL = f"http://apis.data.go.kr/1543061/abandonmentPublicSrvc/abandonmentPublic?bgnde={date}&numOfRows=1000&pageNo={page_number}&serviceKey={API_Key}&_type=json"
     return URL
 
-def get_requests_params():
-    API_Key = os.environ.get("ApiKey")
-    ten_days_ago = (datetime.today()- timedelta(10)).strftime("%Y%m%d")
-    return API_Key, ten_days_ago
+def get_requests_params(key, days_ago):
+    API_Key = os.environ.get(key)
+    date = (datetime.today()- timedelta(days_ago)).strftime("%Y%m%d")
+    return API_Key, date
 
-load_dotenv()
+def get_total_pages(API_Key, date):
+    URL = get_url(API_Key, date)
+    rq = requests.get(URL)
+    data = rq.json()
+    animal_info_totalCount = data['response']['body']['totalCount']
+    animal_info_totalPages = math.ceil(animal_info_totalCount/1000)
+    return animal_info_totalPages
 
-db_regnotest = get_db("regnotest")
-db_openApi = get_db("openApi")
+def get_info_list_by_page(API_Key, date, page_number):
+    URL = get_url(API_Key, date, page_number)
+    rq = requests.get(URL)
+    data = rq.json()
+    info_list = data['response']['body']['items']['item']
+    return info_list
 
-API_Key, ten_days_ago = get_requests_params()
+def get_shelter_info(db):
+    shelter_info_list = list(db.careRegNo.find())
+    shelter_info_dict = {shelter_info['careNm'] : shelter_info['careRegNo'] for shelter_info in shelter_info_list}
+    return shelter_info_dict
+
+def is_good_data(data_dict, key):
+    result = ""
+    try:
+        result = data_dict[key]
+    except:
+        print(key)
+    return result
+
+def main():
+    load_dotenv()
+
+    db_regnotest = get_db("regnotest")
+    db_openApi = get_db("openApi")
+
+    API_Key, date = get_requests_params("ApiKey", 3)
+
+    animal_info_totalPages = get_total_pages(API_Key, date)
+
+    shelter_info_dict = get_shelter_info(db_openApi)
+
+    result = [ 
+        {
+            "desertionNo": info_dict['desertionNo'],
+            "imgUrl": info_dict['popfile'], 
+            "happenDate": info_dict['happenDt'], 
+            "happenPlace": info_dict['happenPlace'], 
+            "kindCode": info_dict['kindCd'], 
+            "colorCode": info_dict['colorCd'], 
+            "sexCode": info_dict['sexCd'], 
+            "neuteY/N": info_dict['neuterYn'], 
+            "noticeStartDate": info_dict['noticeSdt'], 
+            "noticeEndDate": info_dict['noticeEdt'], 
+            "specialMark": info_dict['specialMark'], 
+            "age": info_dict['age'], 
+            "weight": info_dict['weight'], 
+            "processState": info_dict['processState'], 
+            "careCode":  is_good_data(shelter_info_dict,info_dict['careNm']),
+            "careAddr": info_dict['careAddr'], 
+            "careName": info_dict['careNm'], 
+            "careTel": info_dict['careTel'],  
+            "officeTel": info_dict['officetel'] 
+        }
+    for page_number in range(1, animal_info_totalPages+1) 
+    for info_dict in get_info_list_by_page(API_Key, date, page_number)
+        ]
+    db_regnotest.rescues.insert_many(result)
 
 
-URL = get_url(API_Key, ten_days_ago)
-rq = requests.get(URL)
-data = rq.json()
-
-animal_info_list = data['response']['body']['items']['item']
-animal_info_totalCount = data['response']['body']['totalCount']
-animal_info_totalPages = math.ceil(animal_info_totalCount/1000)
-
-
-
+# pprint(result)
 
 # soup = BeautifulSoup(rq.text, "html.parser")
 # totalCnt = int(soup.find("totalcount").text)
