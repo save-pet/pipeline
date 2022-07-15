9fd9d40356b4e88b83e743b2c6b1898d23822796
cyberjam <elicejam@gmail.com>
2022-07-15 16:36:33 +0900

    ✨ happenPlace를 위경도 좌표로 변환 기능 추가

--- openapi.py
+++ openapi.py
@@ -50,6 +50,69 @@ def is_good_data(data_dict, key):
         print(key)
     return result
 
+
+def address2coord(address):
+    preprocess_address = address.split("(")[0]
+    headers = {
+        'Content-Type': 'application/json',
+        'Authorization': f"KakaoAK {os.environ.get('KAKAO_REST_API_KEY')}",
+        'Host': 'dapi.kakao.com',
+    }
+    URL = f"https://dapi.kakao.com/v2/local/search/address.json?query={preprocess_address}"
+
+    rq = requests.get(URL, headers=headers)
+    data = rq.json()
+
+    if len(data['documents'])==0:
+        pprint(data)
+        print(address)
+        print("address2coord Error")
+        return
+        
+    lng = float(data['documents'][0]['address']['x']) # 경도 127.XX
+    lat = float(data['documents'][0]['address']['y']) # 위도 36.xx
+
+    return {"latitude": lat, "longitude": lng}
+
+def place2coord(place):
+    headers = {
+        'Content-Type': 'application/json',
+        'Authorization': f"KakaoAK {os.environ.get('KAKAO_REST_API_KEY')}",
+        'Host': 'dapi.kakao.com',
+    }
+    URL = f"https://dapi.kakao.com/v2/local/search/keyword.json?query={place}"
+
+    rq = requests.get(URL, headers=headers)
+    data = rq.json()['documents']
+    return data
+
+def preprocessing_data(place):
+
+    address = place.split()
+    preprocess_data = [elem for elem in address if '동' in elem or '리' in elem or '면' in elem or '읍' in elem or '시' in elem]
+    if len(preprocess_data)==0:
+        return 0
+    return preprocess_data[-1]
+
+def get_center_coord(data):
+    cunsum_dict = {"lat": 0, "lng": 0}
+
+    for elem in data:
+        cunsum_dict['lat'] += float(elem['y'])
+        cunsum_dict['lng'] += float(elem['x'])
+    result = {"latitude": float(cunsum_dict['lat'])/len(data), "longitude": float(cunsum_dict['lng'])/len(data)}
+    print(result)
+    return result
+
+def get_coord(place, shelter_address):
+    data = place2coord(place)
+    if len(data) == 0:
+        data = place2coord(preprocessing_data(place))
+        if len(data) == 0:
+            return address2coord(shelter_address)
+    
+    return get_center_coord(data)
+
 def main():
     load_dotenv()
 
@@ -70,29 +133,35 @@ def main():
     print("초기화 완료!")
     db_openApi = get_db("openApi")
     shelter_info_dict = get_shelter_info(db_openApi)
-    print("파이프라인 가동!")
+    print(f"{animal_info_totalCount}건 파이프라인 가동!")
     result = [ 
-        {
-            "desertionNo": info_dict['desertionNo'],
-            "imgUrl": info_dict['popfile'], 
-            "happenDate": info_dict['happenDt'], 
-            "happenPlace": info_dict['happenPlace'], 
-            "kindCode": info_dict['kindCd'], 
-            "colorCode": info_dict['colorCd'], 
-            "sexCode": info_dict['sexCd'], 
-            "neuteY/N": info_dict['neuterYn'], 
-            "noticeStartDate": info_dict['noticeSdt'], 
-            "noticeEndDate": info_dict['noticeEdt'], 
-            "specialMark": info_dict['specialMark'], 
-            "age": info_dict['age'], 
-            "weight": info_dict['weight'], 
-            "processState": info_dict['processState'], 
-            "careCode":  is_good_data(shelter_info_dict,info_dict['careNm']),
-            "careAddr": info_dict['careAddr'], 
-            "careName": info_dict['careNm'], 
-            "careTel": info_dict['careTel'],  
-            "officeTel": info_dict['officetel'] 
-        }
+        (
+            lng_lat_dict := get_coord(info_dict['happenPlace'], info_dict['careAddr']),
+            {
+                "desertionNo": info_dict['desertionNo'],
+                "imgUrl": info_dict['popfile'], 
+                "happenDate": info_dict['happenDt'], 
+                "happenPlace": info_dict['happenPlace'], 
+                "kindCode": info_dict['kindCd'], 
+                "colorCode": info_dict['colorCd'], 
+                "sexCode": info_dict['sexCd'], 
+                "neuteY/N": info_dict['neuterYn'], 
+                "noticeStartDate": info_dict['noticeSdt'], 
+                "noticeEndDate": info_dict['noticeEdt'], 
+                "specialMark": info_dict['specialMark'], 
+                "age": info_dict['age'], 
+                "weight": info_dict['weight'], 
+                "processState": info_dict['processState'], 
+                "careCode":  is_good_data(shelter_info_dict, info_dict['careNm']),
+                "careAddr": info_dict['careAddr'], 
+                "careName": info_dict['careNm'], 
+                "careTel": info_dict['careTel'],  
+                "officeTel": info_dict['officetel'],
+                "latitude" : lng_lat_dict['latitude'],
+                "longitude" : lng_lat_dict['longitude']
+            }
+        )[1]
+        
     for page_number in range(1, animal_info_totalPages+1) 
     for info_dict in get_info_list_by_page(API_Key, date, page_number)
         ]
